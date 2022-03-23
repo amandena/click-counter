33db4dcd8d28603fedf9eaa229d41cdd4473b7bb
amandena <manda.m.johnson@gmail.com>
2022-03-22 23:40:03 -0400

    made tests pass

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div data-test='component-app'>
+      <h1 data-test='counter-display'>The counter display is currently</h1>
+      <button data-test='increment-button'>Increment counter</button>
     </div>
   );
 }
