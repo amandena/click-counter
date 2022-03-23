f915738765c6af393a1d526be244399ace6df763
amandena <manda.m.johnson@gmail.com>
2022-03-22 23:39:49 -0400

    added new tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,18 +4,29 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+/** 
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @returns {ShallowWrapper}
+*/ 
+const setup = () => shallow(<App/>)
+
 test('renders without error', () => {
-  const wrapper = shallow(<App/>)
+  const wrapper = setup()
   const appComponent = wrapper.find("[data-test='component-app']")
   expect(appComponent.length).toBe(1)
 })
 
 test('renders increment button', () => {
-
+  const wrapper = setup()
+  const button = wrapper.find("[data-test='increment-button']")
+  expect(button.length).toBe(1)
 })
 
 test('renders counter display', () => {
-
+  const wrapper = setup()
+  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  expect(counterDisplay.length).toBe(1)
 })
 
 test('counter display starts at 0', () => {
