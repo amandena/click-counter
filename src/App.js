ee15a01f60bb0e0a6b00299eb39506a0d763f5c6
amandena <manda.m.johnson@gmail.com>
2022-03-25 14:27:52 -0400

    added code to App to make error tests pass

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [error, setError] = useState('')
 
   return (
     <div data-test='component-app'>
       <h1 data-test='counter-display'>
         The counter display is currently <span data-test='count'>{count}</span>
       </h1>
+      <div data-test='error-message' className={`${error === '' ? 'hidden' : ''}`} style={{color:'red'}}>{error}</div>
       <button 
         data-test='increment-button'
-        onClick={() => setCount(count+1)}
+        onClick={() => {
+          setCount(count+1)
+          setError('')
+        }}
       >
         Increment counter
       </button>
       <button 
         data-test='decrement-button' 
-        onClick={() => setCount(count-1)}
+        onClick={() => count > 0 ? setCount(count-1) : setError('Counter cannot go below zero')}
       >
         Decrement counter
       </button>
